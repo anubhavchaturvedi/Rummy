1d084bd317c999d60997fd10703a1762500f00ca
Rubeenaks <rubeenaks@gmail.com>
2015-07-31 11:21:44 +0530

    working code with basic layout

--- src/Hand.java
+++ src/Hand.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 
 public class Hand {
 
@@ -9,42 +9,14 @@ public class Hand {
 	Hand() {
 		Dealer dealer=new Dealer(3);
 		card=dealer.getHand(13);
-		for(int i=0;i<card.size();i++)
-		{
-			System.out.println(card.get(i));
-		}
-		System.out.println("----");
-		Arrays.sort(card.toArray());
-		for(int i=0;i<card.size();i++)
-		{
-			System.out.println(card.get(i));
-		}
+		Collections.sort(card);		
 		differenceInCards=new ArrayList<Integer>();
 
-		/*card = new ArrayList<Card>();
-		card.add(new Card(5));
-		card.add(new Card(23));
-		card.add(new Card(7));
-		card.add(new Card(2));
-		card.add(new Card(0));
-		Card[] tmp = card.toArray(new Card[0]);
-		System.out.println(Arrays.deepToString(tmp));
-		Arrays.sort(tmp);
-		System.out.println(Arrays.deepToString(tmp));
-		
-		*/
 	}
 	
 	void subtract() {
 		for (int i=0;i<card.size()-1;i++) {
 		differenceInCards.add(card.get(i+1).compareTo(card.get(i)));
-		System.out.println(differenceInCards.get(i));
 		}
-	}
-	
-	public static void main(String args[])
-	{
-		Hand hand=new Hand();
-		hand.subtract();
 	}
 }
