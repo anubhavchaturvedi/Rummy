91ded96db39d40654bf38e730e8537358a81b1ae
Faiz <faiz_84123@yahoo.com>
2015-07-31 10:12:01 +0530

    getHand function of dealer implemented

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Dealer {
-	private int[] deck;
+	private List<Integer> deck;
 	private final int CARDS_IN_PACK = 53;
-	
+
 	public Dealer(int numberOfPack) {
 		int totalCards = CARDS_IN_PACK * numberOfPack;
-		deck = new int[totalCards];
-		for(int i = 0; i < totalCards; i++) {
-			deck[i] = i % CARDS_IN_PACK;
+		deck = new ArrayList<Integer>();
+		for (int i = 0; i < totalCards; i++) {
+			deck.add(i % CARDS_IN_PACK);
 		}
+	}
+
+	public List<Integer> getHand(int length) {
+		Collections.shuffle(deck);
+		return deck.subList(0, length);
 	}
 }
